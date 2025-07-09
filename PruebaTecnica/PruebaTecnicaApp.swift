38f822d76367e6cf2e31ec9653e11d7ca622a9f8
Antonio29blue <anoregon@yosoyblueicon.com>
2025-07-08 19:04:55 -0600

    Initial Commit

---
+++ PruebaTecnica/PruebaTecnicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaTecnicaApp.swift
+//  PruebaTecnica
+//
+//  Created by Developer on 08/07/25.
+//
+
+import SwiftUI
+
+@main
+struct PruebaTecnicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PruebaTecnicaTests/PruebaTecnicaTests.swift
@@ -0,0 +1,16 @@
+//
+//  PruebaTecnicaTests.swift
+//  PruebaTecnicaTests
+//
+//  Created by Developer on 08/07/25.
+//
+
+import Testing
+
+struct PruebaTecnicaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
